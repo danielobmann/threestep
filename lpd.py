1063984b1892e6a2df3795d8a3793ef3345e24ff
dansenob <obmann.daniel@live.de>
2020-08-13 08:41:28 +0200

    ADD: evaluation to training file for convenience

--- lpd.py
+++ lpd.py
@@ -39,6 +39,7 @@ geometry = odl.tomo.Parallel2dGeometry(angle_partition, detector_partition)
 
 operator = odl.tomo.RayTransform(space, geometry)
 pseudoinverse = odl.tomo.fbp_op(operator)
+FBP = odl.tomo.fbp_op(operator, frequency_scaling=0.2)
 
 operator /= odl.operator.power_method_opnorm(operator)
 
@@ -144,10 +145,11 @@ def data_generator_inversion(batch_size=32, mode='train', rescale=1000.):
     p = data_path + mode
     files = np.random.choice(os.listdir(p), size=batch_size, replace=False)
     X = [np.load(p + '/' + file) for file in files]
+    y_n = [operator(x/rescale) for x in X]
     x_t = np.stack(X)[..., None]/rescale
 
     # Get low resolution sinograms
-    y_n = [operator(x) for x in X]
+
     y_n = [y + np.random.normal(0, 1, y.shape) * sigma for y in y_n]
     y_n = np.stack(y_n)[..., None]
 
@@ -234,4 +236,98 @@ plt.semilogy(hist['loss'])
 plt.semilogy(hist['loss_val'])
 plt.savefig('images/lpd_loss.pdf', format='pdf')
 
+
+# ------------------------
+# Plot one specific example for paper
+
+path = "../data/mayoclinic/data/full3mm/test"
+sigma = 0.2
+rescale = 1000.
+
+
+np.random.seed(1)
+file = np.random.choice(os.listdir(path))
+x = np.load(path + '/' + file)/rescale
+y_n = operator(x)
+y_n += np.random.normal(0, 1, y_n.shape)*sigma
+y_n = y_n[None, ..., None]
+
+x_rec = sess.run(output, feed_dict={input_y: y_n, input_x: np.zeros((1, size, size, 1))})
+x_rec = x_rec[0, ..., 0]
+
+plt.imshow(y_n[0, ..., 0], cmap='bone')
+plt.colorbar()
+plt.savefig("images/reconstruction_data.pdf", format='pdf')
+plt.clf()
+
+plt.imshow(x_rec, cmap='bone')
+plt.axis('off')
+plt.savefig('images/reconstruction_lpd.pdf', format='pdf')
+
+plt.imshow(x, cmap='bone')
+plt.axis('off')
+plt.savefig('images/reconstruction_gt.pdf', format='pdf')
+
+plt.imshow(FBP(y_n[0, ..., 0]), cmap='bone')
+plt.axis('off')
+plt.savefig('images/reconstruction_fbp.pdf', format='pdf')
+
+
+# ----------------------------
+# Plot examples
+
+def nmse(x_true, x_pred):
+    d = np.mean((x_true - x_pred)**2)
+    n = np.mean(x_true**2)
+    return d/n
+
+
+def psnr(x_true, x_pred):
+    ran = np.amax(x_true) - np.amin(x_true)
+    return 20*np.log(ran) - 10*np.log(np.mean((x_true - x_pred)**2))
+
+
+NMSE = []
+PSNR = []
+
+
+for file in os.listdir(path):
+    x = np.load(path + '/' + file)/rescale
+    y_n = operator(x)
+    y_n += np.random.normal(0, 1, y_n.shape)*sigma
+    y_n = y_n[None, ..., None]
+
+    x_rec = sess.run(output, feed_dict={input_y: y_n, input_x: np.zeros((1, size, size, 1))})
+    x_rec = x_rec[0, ..., 0]
+
+    NMSE.append(nmse(x, x_rec))
+    PSNR.append(psnr(x, x_rec))
+
+    plt.subplot(221)
+    plt.imshow(x, cmap='bone')
+    plt.axis('off')
+    plt.title('True')
+
+    plt.subplot(222)
+    plt.imshow(pseudoinverse(y_n[0, ..., 0]), cmap='bone')
+    plt.axis('off')
+    plt.title('FBP')
+
+    plt.subplot(223)
+    plt.imshow(x_rec, cmap='bone')
+    plt.axis('off')
+    plt.title('Prediction')
+
+    plt.subplot(224)
+    plt.imshow(np.abs(x - x_rec), cmap='bone')
+    plt.axis('off')
+    plt.colorbar()
+    plt.title('Difference')
+
+    plt.savefig("images/test_lpd/" + file[0:-8] + '.pdf', format='pdf')
+    plt.clf()
+
+
+print(NMSE)
+print(PSNR)
 
--- lpd_evaluation.py
+++ lpd_evaluation.py
@@ -30,6 +30,7 @@ geometry = odl.tomo.Parallel2dGeometry(angle_partition, detector_partition)
 
 operator = odl.tomo.RayTransform(space, geometry)
 pseudoinverse = odl.tomo.fbp_op(operator)
+FBP = odl.tomo.fbp_op(operator, frequency_scaling=0.2)
 
 operator /= odl.operator.power_method_opnorm(operator)
 
@@ -46,9 +47,10 @@ input_y = tf.placeholder(tf.float32, shape=(None, n_theta, n_s, 1), name='input_
 input_x = tf.placeholder(tf.float32, shape=(None, size, size, 1), name='input_x')
 
 
-def apply_conv(inputs, filters=32):
+def apply_conv(inputs, filters=32, act=True):
     outputs = Conv2D(filters, kernel_size=(3, 3), padding='same')(inputs)
-    outputs = PReLU()(outputs)
+    if act:
+        outputs = PReLU()(outputs)
     return outputs
 
 
@@ -65,7 +67,7 @@ with tf.name_scope('tomography'):
 
             update = apply_conv(update)
             update = apply_conv(update)
-            update = apply_conv(update, filters=n_dual)
+            update = apply_conv(update, filters=n_dual, act=False)
             dual = dual + update
 
         with tf.variable_scope('primal_iterate_{}'.format(i)):
@@ -78,7 +80,7 @@ with tf.name_scope('tomography'):
 
             update = apply_conv(update)
             update = apply_conv(update)
-            update = apply_conv(update, filters=n_primal)
+            update = apply_conv(update, filters=n_primal, act=False)
             primal = primal + update
 
     x_result = primal[..., 0:1]
@@ -90,10 +92,16 @@ sess.run(tf.global_variables_initializer())
 # Restore graph from trained model
 restore_path = "models/"
 if 1:
-    new_saver = tf.train.import_meta_graph(restore_path + 'lpd_network-20.meta')
+    new_saver = tf.train.import_meta_graph(restore_path + 'lpd_network-10.meta')
     new_saver.restore(sess, tf.train.latest_checkpoint(restore_path))
 
 graph = tf.get_default_graph()
+
+# my_list = [n.name for n in tf.get_default_graph().as_graph_def().node]
+#
+# with open('your_file.txt', 'w') as f:
+#     for item in my_list:
+#         f.write("%s\n" % item)
 
 inp_inversion = graph.get_tensor_by_name("input_y_1:0")
 inp_x = graph.get_tensor_by_name("input_x_1:0")
@@ -120,8 +128,44 @@ path = "../data/mayoclinic/data/full3mm/test"
 sigma = 0.2
 rescale = 1000.
 
+
+# ------------------------
+# Plot one specific example for paper
+np.random.seed(1)
+file = np.random.choice(os.listdir(path))
+x = np.load(path + '/' + file)/rescale
+y_n = operator(x)
+y_n += np.random.normal(0, 1, y_n.shape)*sigma
+y_n = y_n[None, ..., None]
+
+x_rec = sess.run(out_inversion, feed_dict={inp_inversion: y_n, inp_x: np.zeros((1, size, size, 1))})
+x_rec1 = sess.run(output, feed_dict={input_y: y_n, input_x: np.zeros((1, size, size, 1))})
+
+print(np.mean(x_rec == x_rec1))
+
+x_rec = x_rec[0, ..., 0]
+
+plt.imshow(y_n[0, ..., 0], cmap='bone')
+plt.colorbar()
+plt.savefig("images/reconstruction_data.pdf", format='pdf')
+plt.clf()
+
+plt.imshow(x_rec, cmap='bone')
+plt.axis('off')
+plt.savefig('images/reconstruction_lpd.pdf', format='pdf')
+
+plt.imshow(x, cmap='bone')
+plt.axis('off')
+plt.savefig('images/reconstruction_gt.pdf', format='pdf')
+
+plt.imshow(FBP(y_n[0, ..., 0]), cmap='bone')
+plt.axis('off')
+plt.savefig('images/reconstruction_fbp.pdf', format='pdf')
+
+
 NMSE = []
 PSNR = []
+
 
 for file in os.listdir(path):
     x = np.load(path + '/' + file)/rescale
@@ -164,18 +208,4 @@ print(NMSE)
 print(PSNR)
 
 
-# ------------------------
-# Plot one specific example for paper
-np.random.seed(1)
-file = np.random.choice(os.listdir(path))[0]
-x = np.load(path + '/' + file)/rescale
-y_n = operator(x)
-y_n += np.random.normal(0, 1, y_n.shape)*sigma
-y_n = y_n[None, ..., None]
 
-x_rec = sess.run(out_inversion, feed_dict={inp_inversion: y_n, inp_x: np.zeros((1, size, size, 1))})
-x_rec = x_rec[0, ..., 0]
-
-plt.imshow(x_rec, cmap='bone')
-plt.axis('off')
-plt.savefig('images/reconstruction_lpd.pdf', format='pdf')
