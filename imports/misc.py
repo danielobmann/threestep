164b15705a9afe1d829d6051f843c139bdc41cbd
dansenob <obmann.daniel@live.de>
2020-08-14 10:20:36 +0200

    ADD: numpy implementations of metrics

--- imports/misc.py
+++ imports/misc.py
@@ -24,11 +24,23 @@ def PSNR(x_result, x_true, name='psnr'):
         return 20 * log10(maxval) - 10 * log10(mse)
 
 
+def PSRN_numpy(x_result, x_true):
+    maxval = np.amax(x_true) - np.amin(x_true)
+    mse = np.mean((x_true - x_result)**2)
+    return 20*np.log10(maxval) - 10*np.log10(mse)
+
+
 def NMSE(x_result, x_true, name='nmse'):
     with tf.name_scope(name):
         error = tf.reduce_sum((x_result - x_true) ** 2, axis=[1, 2, 3])
         normalizer = tf.reduce_sum(x_true**2, axis=[1, 2, 3])
         return tf.reduce_mean(error/normalizer)
+
+
+def NMSE_numpy(x_result, x_true):
+    error = np.mean((x_true - x_result)**2)
+    normalizer = np.mean(x_true**2)
+    return error/normalizer
 
 
 class DataGenerator:
