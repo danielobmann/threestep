4e4f72de9cc354ec20b2eb7569d3653363649eb4
c7021101 <obmann.daniel@live.de>
2020-09-22 12:26:33 +0200

    ADD: sobolev loss

--- imports/misc.py
+++ imports/misc.py
@@ -43,6 +43,24 @@ def NMSE_numpy(x_result, x_true):
     return error/normalizer
 
 
+def sob_weight(shape, srange=[-360, 360], beta=0.5):
+    ret = np.zeros(shape)
+    for i in range(shape[0]):
+        ret[i, ...] = (1 + np.linspace(srange[0], srange[1], shape[1])**2)**beta
+    return ret
+
+
+def sob_loss(y_true, y_pred, srange=[-360, 360], beta=0.5):
+    y = tf.cast(y_true - y_pred, tf.complex64)
+    y = tf.squeeze(y)
+    y = tf.fft(y)
+    y = tf.square(tf.abs(y))
+    s = sob_weight(shape = tf.keras.backend.int_shape(y_true)[1:3], srange=srange, beta=beta)
+    s = tf.constant(s, tf.float32)
+    y = y*s
+    return tf.reduce_mean(y)
+
+
 class DataGenerator:
     def __init__(self, operator, operator_up=None, path=data_path, sigma=0.02):
         self._path = path
