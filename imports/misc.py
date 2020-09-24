93e28581c1a40aafd9e7c70fc270125754fe4f85
c7021101 <obmann.daniel@live.de>
2020-09-24 13:53:44 +0200

    ADD: zoomed plot

--- imports/misc.py
+++ imports/misc.py
@@ -1,6 +1,8 @@
 import os
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.inset_locator import *
 
 
 data_path = "../../data/mayoclinic/data/full3mm/"
@@ -59,6 +61,58 @@ def sob_loss(y_true, y_pred, srange=[-360, 360], beta=0.5):
     s = tf.constant(s, tf.float32)
     y = y*s
     return tf.reduce_mean(y)
+
+
+def _mark_inset(parent_axes, inset_axes, **kwargs):
+    # This code is copied from the matplotlib source code and slightly modified.
+    # This is done to avoid the 'connection lines'.
+    rect = TransformedBbox(inset_axes.viewLim, parent_axes.transData)
+
+    if 'fill' in kwargs:
+        pp = BboxPatch(rect, **kwargs)
+    else:
+        fill = bool({'fc', 'facecolor', 'color'}.intersection(kwargs))
+        pp = BboxPatch(rect, fill=fill, **kwargs)
+    parent_axes.add_patch(pp)
+
+    p1 = BboxConnector(inset_axes.bbox, rect, loc1=1, **kwargs)
+    p1.set_clip_on(False)
+
+    p2 = BboxConnector(inset_axes.bbox, rect, loc1=1, **kwargs)
+    p2.set_clip_on(False)
+
+    return pp, p1, p2
+
+
+def zoomed_plot(x, xlim, ylim, zoom=2, text=None, textloc=[], fsize=18, cmap='bone'):
+
+    # This function allows one to create plots with "zoomed in" windows.
+    # The rectangle where one desires to zoom in is given using the xlim and ylim arguments.
+    # xlim and ylim should contain pixel values, e.g. if we haven an image of size 512 x 512 then
+    # xlim = [100, 150] and ylim = [100, 150] shows a zoomed in version of the pixels at locations in xlim and ylim.
+
+    color = 'orange'
+    fig, ax = plt.subplots()
+    ax.imshow(np.flipud(x), cmap=cmap, origin="lower")
+    ax.axis('off')
+
+    axins = zoomed_inset_axes(ax, zoom, loc=4)
+
+    axins.set_xlim(xlim[0], xlim[1])
+    axins.set_ylim(ylim[0], ylim[1])
+
+    _mark_inset(ax, axins, fc='none', ec=color)
+
+    axins.imshow(np.flipud(x), cmap=cmap, origin="lower")
+    axins.patch.set_edgecolor(color)
+    axins.patch.set_linewidth('3')
+    axins.set_xticks([], [])
+    axins.set_yticks([], [])
+    # axins.axis('off')
+
+    if not (text is None):
+        ax.text(textloc[0], textloc[1], text, color=color, fontdict={'size': fsize}, transform=ax.transAxes)
+    pass
 
 
 class DataGenerator:
